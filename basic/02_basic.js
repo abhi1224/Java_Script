1a635273dc00d41a6811fe52af0701495b0044f6
Abhishek Verma <99130950+abhi1224@users.noreply.github.com>
2023-10-18 02:38:45 +0000

    data type

---
+++ basic/02_basic.js
@@ -0,0 +1,20 @@
+const accoutnId = 123
+let accountMail = "test@gmail.com"
+var accountPass = "1234"
+accoutnCity = "jaipur"
+
+
+/*accoutnId = 345  this is not allowed becaue we are using const */
+
+var accountPass = "456"
+/*
+Prefer not to use var because of the issue in block scope and functional scope
+ */
+console.log(accountPass)
+
+console.table([accoutnId,accountMail,accountPass,accoutnCity])
+
+
+let accountState; // undefined
+
+console.log(typeof(accountState))
