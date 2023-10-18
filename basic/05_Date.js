a1a2500d9fd024c14a93b8d7da22f2569641fc2f
Abhishek Verma <99130950+abhi1224@users.noreply.github.com>
2023-10-18 05:40:02 +0000

    Date and its methods

---
+++ basic/05_Date.js
@@ -0,0 +1,23 @@
+let myDate = new Date()
+
+console.log(typeof myDate) // object
+
+
+console.log(myDate.toString())
+// console.log(myDate.toDateString()) 
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.toTimeString())
+// getDay , getMonth , getYear
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+console.log(Math.floor(Date.now() / 1000))
+
+
+
