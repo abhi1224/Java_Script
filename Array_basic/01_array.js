8c803c99e19e550451a36c8fc406aa95ce212989
Abhishek Verma <99130950+abhi1224@users.noreply.github.com>
2023-10-18 06:04:13 +0000

    Array basics part 1

---
+++ Array_basic/01_array.js
@@ -0,0 +1,27 @@
+// Array
+
+const arr  = [ 0 , 1 , 2 , 'True' , 'False']
+
+const myArr = new Array(0,1,2,3,4)
+console.log(myArr);
+// myArr.push(6)
+// myArr.push(7)
+//  myArr.unshift(9)
+
+
+// console.log(myArr);
+
+// myArr.shift() 
+// console.log(myArr);
+// myArr.pop()     // remove the last element 
+
+// const newArr = myArr.join()
+// console.log(newArr)
+
+// slice , splice 
+
+ const myNum = myArr.slice(1,3)
+ console.log(myNum);
+
+ const myNum2 = myArr.splice(1,3) // manupulate the original array 
+ console.log(myNum);
