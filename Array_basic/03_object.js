c75ecda77e07374edc0609a2cd3ee581d0ff7780
Abhishek Verma <99130950+abhi1224@users.noreply.github.com>
2023-10-21 16:46:28 +0000

    basics of object and object literals

---
+++ Array_basic/03_object.js
@@ -0,0 +1,20 @@
+
+// JavaScript variables are containers for data values. Objects are variables too, but they can contain many values.
+
+// Think of an object as a list of values that are written as name:value pairs, with the names and the values separated by colons.
+
+
+// === object literal===
+
+let person = {
+    name: "Robert",
+    age:32,
+    favColor: "blue",
+    height: 123
+}
+
+
+// accessing object elemet using objectName.methodName()
+console.log(person.favColor)
+// or
+console.log(person['favColor'])
