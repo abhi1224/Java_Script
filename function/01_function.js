27ce224fc9b84054fb3d3df22645a4252116d871
Abhishek Verma <99130950+abhi1224@users.noreply.github.com>
2023-10-24 05:57:18 +0000

    function basic

---
+++ function/01_function.js
@@ -0,0 +1,34 @@
+
+function sayMyName(){
+    console.log('a');
+    console.log('b');
+    console.log('h');
+    console.log('i');
+    console.log('s');
+    console.log('h');
+    console.log('e');
+    console.log('k');
+}
+
+// sayMyName() // calling function
+
+function addTwoNumber(number1 , number2)
+{
+    console.log(number1 + number2)
+}
+
+function sum(number1 , number2)
+{
+    return number1 + number2
+}
+
+// addTwoNumber(3,5)
+// const result  = sum(3,5)
+// console.log("Result: " , result);  
+
+function name (firstname = 'abhi')
+{
+    console.log(firstname)
+}
+name('verma')
+name()
