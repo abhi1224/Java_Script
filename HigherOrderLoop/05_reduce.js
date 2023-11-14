5d5c06d4377408d710c240c208d70971bd3e38a1
Abhishek Verma <99130950+abhi1224@users.noreply.github.com>
2023-11-14 03:50:49 +0000

    reduce

---
+++ HigherOrderLoop/05_reduce.js
@@ -0,0 +1,33 @@
+  const myNums = [1,2,3]
+
+//  const myTotal =  myNums.reduce(function(acc , currentvalue) {
+//     console.log(`acc:${acc} and currentvalue:${currentvalue}`);
+//     return acc + currentvalue
+//  }, 0) 
+// acc:0 and currentvalue:1
+//  acc:1 and currentvalue:2
+//  acc:3 and currentvalue:3
+
+
+// const myTotal = myNums.reduce((acc , curr) => {return acc + curr},0)
+
+//  console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName:'js course',
+        price:1000
+    },
+    {
+        itemName:'py course',
+        price:2000
+    },
+    {
+        itemName:'data science course',
+        price:7000 
+    },
+    
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price ,0)
+console.log(priceToPay);
