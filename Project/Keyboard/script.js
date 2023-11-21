1e06d8cc1ce1d7e098d5bf0374816768cab921fe
Abhishek Verma <99130950+abhi1224@users.noreply.github.com>
2023-11-21 05:05:34 +0000

    Key Board

---
+++ Project/Keyboard/script.js
@@ -0,0 +1,20 @@
+const value = document.querySelector('.key')
+window.addEventListener('keydown' , function(e){
+    value.innerHTML = `
+    <div class="color">
+        <table>
+            <tr>
+                <th>Key</th>
+                <th>Key Code </th>
+                <th>Code</th>
+            </tr>
+            <tr>
+                <td>${e.key === ' '? 'Space' : e.key}</td>
+                <td>${e.keyCode}</td>
+                <td>${e.code}</td>
+            </tr>
+        </table>
+    </div>
+
+    `
+})
