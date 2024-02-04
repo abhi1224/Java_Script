bff2e5d0e1167f80cc65ad64ad6c8e427470c2a8
Abhishek Verma <99130950+abhi1224@users.noreply.github.com>
2024-02-04 15:33:31 +0000

    higher order function

---
+++ function/higher_function.js
@@ -0,0 +1,11 @@
+
+
+// higher order function(add)  cb- call back
+function add( a, b , cb) {
+    let result =  a + b
+    cd(result)
+}
+
+add(2, 4, (res) => console.log(res))
+
+// passing function to function 
