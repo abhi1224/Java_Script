310c38031ffc93ca3834e3507c14269c31121818
Abhishek Verma <99130950+abhi1224@users.noreply.github.com>
2024-02-07 01:37:50 +0000

    currying in javascript (using multiple closures)

--- closure/script.js
+++ closure/script.js
@@ -13,3 +13,33 @@ function userName(name) {
 let closure = userName('Abhishek')
 closure()
 
+// curryring in javascript
+
+// function add(a) {
+//     return function (b) {
+//         return function (c) {
+//             return a + b + c
+//         }
+//     }
+// }
+
+const add = a => b => c => a + b + c;
+
+console.log(add(2)(3)(5));
+
+// function sendingEmail(to){
+//     return function(subject){
+//         return function (body){
+//             console.log(`Sending Email to ${to} with subject ${subject}: ${body}`);
+//         }
+//     }
+// }
+
+// ES6 syntax
+const sendingEmail = to => subject => body => console.log(`Sending Email to ${to} with subject ${subject}: ${body}`);
+
+let step1 = sendingEmail('abhishek27501@gmail.com')
+let step2 = step1('New Order Conformation')
+step2('Hey Abhishek ! Here is something for you.')
+
+
